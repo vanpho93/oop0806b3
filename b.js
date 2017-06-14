88df90f58adc52b054a936cd86b915291563d678
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-14 09:07:32 +0700

    class

--- b.js
+++ b.js
@@ -2,7 +2,10 @@ function Empolyee (ten, tuoi) {
     this.name = ten;
     this.age = tuoi;
     this.sayHello = function() {
-        console.log('Hello');
+        console.log('Hello, I am ' + this.name);
+    }
+    this.getBornYear = function() {
+        return new Date().getFullYear() - this.age;
     }
 }
 
@@ -11,3 +14,8 @@ const khoa = new Empolyee('Khoa', 30);
 // console.log(pho.name);
 // console.log(khoa);
 pho.sayHello();
+khoa.sayHello();
+console.log(pho.getBornYear());
+console.log(khoa.getBornYear());
+
+// console.log(new Date().getFullYear());
